824df709f91cef00df3884ae04f3618ba58d06ab
Ideveloper2 <ideveloper2@Ideveloper2s-iMac.local>
2018-12-01 12:56:56 +0530

    Initial Commit

---
+++ radioTests/radioTests.swift
@@ -0,0 +1,34 @@
+//
+//  radioTests.swift
+//  radioTests
+//
+//  Created by Ideveloper2 on 01/12/18.
+//  Copyright © 2018 Ideveloper2. All rights reserved.
+//
+
+import XCTest
+@testable import radio
+
+class radioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
