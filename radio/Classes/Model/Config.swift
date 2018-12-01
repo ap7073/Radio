66565c80a41e2020b356e4328f6743172d011fde
Ideveloper2 <ideveloper2@Ideveloper2s-iMac.local>
2018-12-01 15:09:53 +0530

    first commit

---
+++ radio/Classes/Model/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  radio
+//
+//  Created by Ideveloper2 on 01/12/18.
+//  Copyright © 2018 Ideveloper2. All rights reserved.
+//
+
+import UIKit
+
+class Config: NSObject {
+
+}
---
+++ radio/Classes/Views/RadioListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RadioListTableViewCell.swift
+//  radio
+//
+//  Created by Ideveloper2 on 01/12/18.
+//  Copyright © 2018 Ideveloper2. All rights reserved.
+//
+
+import UIKit
+
+class RadioListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
